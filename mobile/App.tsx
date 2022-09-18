2d4db4f6a40c24b749f6e5a3a5ccdf0f2ff5a5c2
r-avellar <renatoavellar5@gmail.com>
2022-09-18 00:23:06 -0300

    criando notificações e finalizando mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,42 @@
+import { useRef, useEffect } from 'react';
 import { Background } from './src/components/Background';
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold ,Inter_900Black} from '@expo-google-fonts/inter'
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
-
+import './src/services/notificationConfigs';
+import { getPushNotificationToken } from './src/services/getPushNotificationToken';
+import { Subscription } from 'expo-modules-core';
+import * as Notification from 'expo-notifications';
 
 
 export default function App() {
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  });
+  
+  useEffect(()=>{
+    getNotificationListener.current = Notification.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    });
+
+
+    responseNotificationListener.current = Notification.addNotificationResponseReceivedListener(response => {
+      console.log(response)
+    })
+
+    return () => {
+      if(getNotificationListener.current && responseNotificationListener.current){
+        Notification.removeNotificationSubscription(getNotificationListener.current);
+        Notification.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    } 
+  }, []);
+
+
   const [fontsLoad] = useFonts({
     Inter_400Regular, Inter_600SemiBold, Inter_700Bold ,Inter_900Black 
   });
